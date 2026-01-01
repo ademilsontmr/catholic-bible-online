b0513cceec9642a9e9271a601274fee5d774c350
Ademilson Ramos <ademilsonr@MacBook-Air-de-Ademilson.local>
2026-01-01 11:16:54 -0300

    Remove Verse of the Day section from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import type { Metadata } from 'next'
 import SearchBox from '@/components/SearchBox'
 import BlogCard from '@/components/BlogCard'
-import VerseOfTheDay from '@/components/VerseOfTheDay'
 import blogData from '@/data/blog.json'
 
 export const metadata: Metadata = {
@@ -69,9 +68,6 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-
-      {/* Verse of the Day Section */}
-      <VerseOfTheDay />
 
       {/* Featured Blog Posts Section */}
       <section className="py-16 bg-gradient-to-r from-blue-50 via-purple-50 to-emerald-50">
